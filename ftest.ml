5e6e2aa5b0598ca4c1d5429565b6da0d2f5ac1c1
M. Dandin Brieuc <dandin@etud.insa-toulouse.fr>
2018-11-06 15:05:40 +0100

    Premier test

--- ftest.ml
+++ ftest.ml
@@ -1,5 +1,26 @@
 open Graph
 
+(** Tests *)
+
+let test_graph_map infile outfile = 
+  (* Open file *)
+  let graph = Gfile.from_file infile in
+
+  let graph_int = Graph.map graph int_of_string in
+
+  let graph_int2 = Graph.map graph_int (fun i -> 2 * i) in
+  
+  let graph_reversed = Graph.map graph_int2 string_of_int in
+
+  (* Rewrite the graph that has been read. *)
+  let () = Gfile.export outfile graph_reversed in
+
+  ()
+
+
+
+(** Main *)
+
 let () =
 
   if Array.length Sys.argv <> 5 then
@@ -15,11 +36,7 @@ let () =
   and _source = Sys.argv.(2)
   and _sink = Sys.argv.(3)
   in
-
-  (* Open file *)
-  let graph = Gfile.from_file infile in
-
-  (* Rewrite the graph that has been read. *)
-  let () = Gfile.export outfile graph in
-
+  
+  let () = test_graph_map infile outfile in
   ()
+
